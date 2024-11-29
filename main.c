a55f7472b38043a0d434e3b0f414db3195c260d2
Wattiez Nicolas <wattieznicolas@MacBook-Pro-de-Nicolas.local>
2024-11-29 23:58:53 +0100

    Depot des fichiers

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct animal
+{
+    char name[20];
+    char espece[20];
+    char categorie[20];
+
+} animal;
+
+int main()
+{
+    return 0;
+}
